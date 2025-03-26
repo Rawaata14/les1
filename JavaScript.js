6ac09d73b0b6aba8833cc06a65d7a33f9b40b964
Rawaata14 <rawatareef1@gmail.com>
2025-03-26 12:41:00 +0200

    upload - alert_Task#1

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,11 @@
-const myNamearry = ["Rawaa","Tareef"]
-console.log(myNamearry)
+// //Task1
+// document.writeln("Rawaa Tareef")
+// //Task2
+// const myName = { firstname: "Rawaa", lastname: "Tareef" }
+// console.log(myName)
+// //Task3
+// const myNamearry = ["Rawaa","Tareef"]
+// console.log(myNamearry)
+//alert - Task#1
+let name = prompt('Enter your name');
+alert(`your name is ${name}`)
